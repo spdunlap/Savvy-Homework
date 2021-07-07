06aaebb37bac41952d337f1e0ffff0adb3ee3069
Spencer Dunlap <spencerdunlap@me.com>
2021-07-06 19:42:57 -0500

    Data Coercion Intro

---
+++ classActivities/2.3.2-DataCoercionIntro.js
@@ -0,0 +1,11 @@
+// ? what should we expect to print?
+
+console.log("2" + 2); // 22
+console.log(2 + "2"); // 22
+console.log(2 + 2); // 4
+console.log("2" + "2"); // 22
+console.log(true + true); // 2; (True = 1; False = 0)
+console.log(2 + true); // 3
+
+console.log(true + true + "true" + "false"); // 2truefalse
+console.log(1 + 1 + "2" + "3"); // 223
