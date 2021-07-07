b4d6d7cc537d547a433e98db3a884004d958c8b9
Spencer Dunlap <spencerdunlap@me.com>
2021-07-06 20:40:19 -0500

    If Else Statements

---
+++ classActivities/2.3.5-MoreIfElseStatements.js
@@ -0,0 +1,16 @@
+// replace the 1 with different numbers to test different conditions let num = 1;
+
+// ? fill in all the parentheses with conditions to log the correct statements
+let num = 34;
+
+if (num > 100) {
+  console.log("The number is greater than 100.");
+} else if (num <= 40) {
+  console.log("The number is less than or equal to 40");
+} else if (num === 42) {
+  console.log("The number is 42");
+} else if (num >= 45) {
+  console.log("The number is greater than or equal to 45.");
+} else {
+  console.log("The number is one of these three numbers: 41, 43, or 44.");
+}
