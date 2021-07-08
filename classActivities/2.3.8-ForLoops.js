9b312958dd0fa3975e3a4b9103c7d5764b7acb02
Spencer Dunlap <spencerdunlap@me.com>
2021-07-07 19:17:11 -0500

    For Loops

---
+++ classActivities/2.3.8-ForLoops.js
@@ -0,0 +1,7 @@
+// ? TO DO: create a for loop that TOTALS the numbers from 1 to 100
+let total = 0; // This must go outside of the code block or it will set total to 0 each time.
+
+for (let i = 1; i <= 100; i++) {
+  total += i;
+  console.log(total);
+}
