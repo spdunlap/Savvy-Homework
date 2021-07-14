63462b228274045930c4cb32cf097996c19b6335
Spencer Dunlap <spencerdunlap@me.com>
2021-07-14 15:35:51 -0500

    homeworkBonue

--- homeworkFiles/homeworkWeek3Bonus.js
+++ homeworkFiles/homeworkWeek3Bonus.js
@@ -10,12 +10,16 @@ let pizzaToppings = [
   " green pepper"
 ];
 
+let lastTopping = pizzaToppings.pop();
+
+//let b = preparePizza();
+//let c = servePizza();
+
 // 2. Create a greetCustomer function that prints a message that welcomes a guest, then informs them of the available toppings by looping over pizzaToppings (don't worry about perfect grammar here yet, i.e. "a, b, and c", see Bonus Challenge #9)
 // - i.e. "Welcome to Pizza House, our toppings are: a, b, c, ..."
 function greetCustomer() {
   console.log(
-    "Welcome to A Whole Lotta Pie! Choose from any of our 45 toppings including the classics: " +
-      pizzaToppings
+    `Welcome to A Whole Lotta Pie! Choose from any of our 45 toppings including the classics: ${pizzaToppings} and ${lastTopping}!`
   );
 }
 
@@ -23,10 +27,23 @@ function greetCustomer() {
 // - has the parameters size, crust, and an indefinite amount of toppings as inputs
 // - prints the order, i.e. "One large thick crust pizza with x, y, z, ... coming up!"
 // - outputs a list with the size, crust, and toppings
+
+let order = [];
+
 function getPizzaOrder(size, crust, ...pizzaToppings) {
-  console.log(
-    `One ${size} ${crust} pizza pie with ${pizzaToppings} headed to the oven!`
-  );
+  let lastSelectedTopping = pizzaToppings.pop();
+  let selection = pizzaToppings;
+  if (typeof selection === "string") {
+    console.log(`One ${size} ${crust} pizza pie headed to the oven!`);
+  } else if (typeof selection !== "string") {
+    console.log(
+      `One ${size} ${crust} with ${pizzaToppings} and ${lastSelectedTopping} headed to the oven!`
+    );
+  }
+
+  order.push(size, crust, [...pizzaToppings, lastSelectedTopping]);
+  //console.log("this is the order", order[2]);
+  return order;
 }
 
 // 4. Create a preparePizza function that
@@ -34,9 +51,18 @@ function getPizzaOrder(size, crust, ...pizzaToppings) {
 // - prints something like "...Cooking pizza..."
 // - outputs a pizza Object with appropriate key-value pairs for size, crust, and toppings
 
-function preparePizza([size, crust, [...pizzaToppings]]) {
+let object = {};
+
+function preparePizza(order) {
   console.log("Thatsa one hot-n-tasty pizza pie baking up!");
-  console.log([size, crust, [...pizzaToppings]]);
+  object = {
+    orderNumber: "451",
+    size: order[0],
+    crust: order[1],
+    toppings: order[2]
+  };
+  //console.log("object", object);
+  return object;
 }
 
 // 5. Create a servePizza function that
@@ -45,22 +71,27 @@ function preparePizza([size, crust, [...pizzaToppings]]) {
 // - outputs the same pizza Object that was passed in
 
 function servePizza(pizzaOrder) {
+  // finalOrder = {
+  //   size: pizzaOrder.size,
+  //   crust: pizzaOrder.crust,
+  //   toppings: pizzaOrder.toppings
+  // }
+
+  let servedTopping = pizzaOrder.toppings.pop();
+
   console.log(
-    `Hot-n-tasty pizza pie ready for consumption! This ${pizzaOrder.size} ${pizzaOrder.crust} tasty pie's got ${pizzaOrder.toppings}. Enjoy!`
+    `Hot-n-tasty pizza pie ready for consumption! This ${pizzaOrder.size} ${pizzaOrder.crust} tasty pie's got ${pizzaOrder.toppings}, and ${servedTopping}. Enjoy!`
   );
+  pizzaOrder.toppings.push(servedTopping);
   console.log(pizzaOrder);
+  //return pizzaOrder;
 }
 
 // 6. Call each function and (starting with preparePizza) use the returned value from the previous function as its input
 greetCustomer();
-getPizzaOrder("small", "Chicago Style", "cheese", " pepperoni");
-preparePizza(["MONSTER", "Deep Dish", ["cheese", "pepperoni", "sausage"]]);
-servePizza({
-  orderNumber: "451",
-  size: "MONSTER",
-  crust: "Deep Dish",
-  toppings: ["cheese", " pepperoni", " sausage"]
-});
+getPizzaOrder("small", "Chicago Style", "Cheese", "Pepperoni");
+preparePizza(order);
+servePizza(object);
 
 // 7. Add, commit, and push your JS file to your GitHub repo.
 
